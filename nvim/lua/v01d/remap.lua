8a0df5d2e185319d0c78a1dbd91cbeea2e0c3ede
V01Ddev <x.v.0.1.d.xhacking@gmail.com>
2023-12-02 22:28:14 +0300

    Added yank into clipboard

--- nvim/lua/v01d/remap.lua
+++ nvim/lua/v01d/remap.lua
@@ -8,4 +8,9 @@ nnoremap <Leader>p "+p
 nnoremap <Leader>P "+P
 vnoremap <Leader>p "+p
 vnoremap <Leader>P "+P
+
+nnoremap <Leader>Y "+Y
+nnoremap <Leader>y "+y
+vnoremap <Leader>Y "+Y
+vnoremap <Leader>y "+y
 ]])
