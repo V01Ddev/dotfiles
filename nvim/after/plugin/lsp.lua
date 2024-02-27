227f74beec79e53efa600364bbf04ab664a5a61e
v01d.dev <x.v.0.1.d.xhacking@gmail.com>
2024-02-27 12:03:14 +0300

    Fixed treesitter and lsp + Removed uneeded LSPs

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,8 +2,8 @@ require("mason").setup()
 require("mason-lspconfig").setup({
     ensure_installed = {
         "pylsp",
-        "ltex",
-        "arduino_language_server"
+        "clangd",
+        "ltex"
     }
 })
 
--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,14 +1,15 @@
 require'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "python", 
-    "cpp",
-    "c",
-    "bash",
-    "arduino"},
+    ensure_installed = {
+        "python", 
+        "c",
+        "cpp",
+        "bash",
+        "latex"},
 
     -- Automatically install missing parsers when entering buffer
     -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,
+    auto_install = false,
 
     highlight = {
         -- `false` will disable the whole extension
@@ -16,5 +17,5 @@ require'nvim-treesitter.configs'.setup {
         additional_vim_regex_highlighting = false,
     }, 
     -- enable indentation
-      indent = { enable = true },
+    indent = { enable = true },
 }
