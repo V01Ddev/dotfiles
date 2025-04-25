e47a80dd9f89102fa969c9330251c6566dcb166c
v01d.dev <v01d.dev.sh@gmail.com>
2025-04-25 22:44:25 +0300

    feat: added ruff and refreshed list

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,10 +2,11 @@ require("mason").setup()
 require("mason-lspconfig").setup({
     ensure_installed = {
         "pylsp",
+        "ruff",
         "clangd",
         "phpactor",
-        "eslint",
-        "ltex",
+        "html",
+        "texlab",
     }
 })
 
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/v01d/.cache/nvim/packer_hererocks/2.1.1727870382/share/lua/5.1/?.lua;/home/v01d/.cache/nvim/packer_hererocks/2.1.1727870382/share/lua/5.1/?/init.lua;/home/v01d/.cache/nvim/packer_hererocks/2.1.1727870382/lib/luarocks/rocks-5.1/?.lua;/home/v01d/.cache/nvim/packer_hererocks/2.1.1727870382/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/v01d/.cache/nvim/packer_hererocks/2.1.1727870382/lib/lua/5.1/?.so"
+local package_path_str = "/home/v01d/.cache/nvim/packer_hererocks/2.1.1741730670/share/lua/5.1/?.lua;/home/v01d/.cache/nvim/packer_hererocks/2.1.1741730670/share/lua/5.1/?/init.lua;/home/v01d/.cache/nvim/packer_hererocks/2.1.1741730670/lib/luarocks/rocks-5.1/?.lua;/home/v01d/.cache/nvim/packer_hererocks/2.1.1741730670/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/v01d/.cache/nvim/packer_hererocks/2.1.1741730670/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -143,16 +143,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/v01d/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
-  },
-  ["vim-airline"] = {
-    loaded = true,
-    path = "/home/v01d/.local/share/nvim/site/pack/packer/start/vim-airline",
-    url = "https://github.com/vim-airline/vim-airline"
-  },
-  ["vim-airline-themes"] = {
-    loaded = true,
-    path = "/home/v01d/.local/share/nvim/site/pack/packer/start/vim-airline-themes",
-    url = "https://github.com/vim-airline/vim-airline-themes"
   },
   vimtex = {
     loaded = true,
