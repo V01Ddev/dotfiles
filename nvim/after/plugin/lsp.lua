c32c5360c1da61d3d5e3ec8eefc1e17567927e87
v01d.dev <v01d.dev.sh@gmail.com>
2025-04-27 10:22:51 +0300

    fix: setup all LSPs... Added bash

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,12 +2,31 @@ require("mason").setup()
 require("mason-lspconfig").setup({
     ensure_installed = {
         "pylsp",
-        "ruff",
         "clangd",
+        "bashls",
         "phpactor",
         "html",
         "texlab",
     }
 })
 
-require("lspconfig").clangd.setup{}
+-- Setup lspconfig
+local lspconfig = require("lspconfig")
+
+-- Setup clangd
+lspconfig.clangd.setup({})
+
+-- Setup bashls
+lspconfig.bashls.setup({})
+
+-- Setup pylsp
+lspconfig.pylsp.setup({})
+
+-- Setup phpactor
+lspconfig.phpactor.setup({})
+
+-- Setup html
+lspconfig.html.setup({})
+
+-- Setup texlab
+lspconfig.texlab.setup({})
