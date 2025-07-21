8f1af51f71eddcd9672aa851bd70b2626b558a36
v01d.dev <v01d.dev.sh@gmail.com>
2025-07-21 17:20:57 +0300

    fix: better printing and path fix

--- scripts/azure_utils/azure_env.py
+++ scripts/azure_utils/azure_env.py
@@ -1,32 +1,41 @@
 #!/bin/env python
 import argparse
-import json
+import re
 import os
 
 
 def check_missing_vars(fp1, dict1, fp2, dict2):
+    print('Checking for missing variables:')
     for var_name in dict1:
-        if not dict2[var_name]:
-            print(f"{var_name} from {fp1} not found in {fp2}")
+        val = dict2.get(var_name, None)
+        if val is None:
+            print(f"\t{var_name} from {fp1} not found in {fp2}")
+
+    for var_name in dict2:
+        val = dict1.get(var_name, None)
+        if val is None:
+            print(f"\t{var_name} from {fp2} not found in {fp1}")
 
 
 def check_values(fp1, dict1, fp2, dict2):
+    print('Checking for differing variable values:')
     for var_name in dict1:
         val1 = dict1.get(var_name)
         val2 = dict2.get(var_name)
 
         if val1 != val2:
-            print(f"{var_name} differs;")
-            print(f"{fp1}: {val1}")
-            print(f"{fp2}: {val2}")
+            print(f"\t{var_name} differs;")
+            print(f"\t{fp1}: {val1.strip()}")
+            print(f"\t{fp2}: {val2.strip()}")
+            print()
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("env_one", help="first path to env")
     parser.add_argument("env_two", help="second path to env")
-    parser.add_argument("-cn", "--compare-name")
-    parser.add_argument("-cv", "--compare-cv")
+    # parser.add_argument("-cn", "--compare-name")
+    # parser.add_argument("-cv", "--compare-cv")
     args = parser.parse_args()
 
     file1 = args.env_one
@@ -46,6 +55,7 @@ def main():
 
     with open(file1, 'r') as file1_data:
         for line in file1_data:
+            line = re.sub(r'#.*', '', line)  # Parsing out comments
             line_data = line.split('=')
             var_name = line_data[0]
             var_value = line_data[1]
@@ -53,18 +63,19 @@ def main():
 
     with open(file2, 'r') as file2_data:
         for line in file2_data:
+            line = re.sub(r'#.*', '', line)  # Parsing out comments
             line_data = line.split('=')
             var_name = line_data[0]
             var_value = line_data[1]
             env_data2[var_name] = var_value
 
-    print('Checking for missing variables')
     check_missing_vars(
         file1, env_data1,
         file2, env_data2
     )
 
-    print('Checking for differing variable values')
+    print("------------------------------")
+
     check_values(
         file1, env_data1,
         file2, env_data2
