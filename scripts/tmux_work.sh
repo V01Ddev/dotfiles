1cac92ee82dda26571f5d241fe61f5f93602823d
v01d.dev <v01d.dev.sh@gmail.com>
2026-02-03 10:20:33 +0300

    feat: working new tmux work session script

--- scripts/tmux_work.sh
+++ scripts/tmux_work.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 window_name=""
-project_path=""
+project_path="$HOME/Work/..."
 
-tmux new -d -s $window_name
-tmux send-keys -t   $window_name:1 'cd $project_path' C-m
-tmux send-keys -t   $window_name:1 '. .venv/bin/activate' C-m
-tmux new-window -t  $window_name:2
-tmux send-keys -t   $window_name:2 'cd $project_path' C-m
+tmux new-session -d -s "$window_name"
+
+tmux send-keys -t "$window_name:1" "cd $project_path" C-m
+tmux send-keys -t "$window_name:1" "source .venv/bin/activate" C-m
+
+tmux new-window -t "$window_name:2"
+tmux send-keys -t "$window_name:2" "cd $project_path" C-m
